450c33382039bfc0ba3055076d6859931224994c
Gabriel Bento da Silva <67703889+HDG-Gabriel@users.noreply.github.com>
2021-10-04 17:42:13 -0400

    Catch errors

--- lib/components/banner_video.dart
+++ lib/components/banner_video.dart
@@ -1,81 +1,148 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_downloader/styles/banner_video.dart' as style;
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 class BannerVideo extends StatelessWidget {
+  String url;
+
+  BannerVideo({required this.url});
+
+  String formatedTime(Duration time) {
+    final hours = time.inHours;
+    if (hours == 0) {
+      final minutes = time.inMinutes;
+      final seconds = time.inSeconds - minutes * 60;
+      return '$minutes:${seconds.toString().padLeft(2, '0')}';
+    } else {
+      final minutes = time.inMinutes - hours * 60;
+      final seconds = time.inSeconds - hours * 3600 - minutes * 60;
+      return '$hours:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    const title = 'Berserk 1997 edit || Little Dark Age';
-    const channel = 'Izanaghi';
-    const id = '06p2RUP-HqE';
+    var yt = YoutubeExplode();
 
-    const url = 'https://i.ytimg.com/vi/$id/0.jpg';
+    final width = MediaQuery.of(context).size.width;
 
-    final downloadButton = Container(
-      decoration: const BoxDecoration(
-        color: style.BannerVideo.backgroundIconDownload,
-        shape: BoxShape.circle,
-      ),
-      child: IconButton(
-        color: style.BannerVideo.colorIconDownload,
-        onPressed: () {},
-        icon: const Icon(Icons.download),
-      ),
-    );
+    return FutureBuilder(
+        future: yt.videos.get(url),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var video = snapshot.data as Video;
 
-    final left = Expanded(
-      flex: 3,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(title, style: style.BannerVideo.title),
-          const SizedBox(height: 10),
-          Text(channel, style: style.BannerVideo.channel),
-        ],
-      ),
-    );
-
-    final right = Expanded(
-      child: Column(
-        children: [
-          downloadButton,
-        ],
-      ),
-    );
-
-    return Container(
-      width: 480,
-      height: 220,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        image: const DecorationImage(
-          image: NetworkImage(url),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Expanded(
-            child: Container(
+            final timeBox = Container(
+              padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 3),
               decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(25),
+                color: Colors.black.withOpacity(0.7),
+                borderRadius: BorderRadius.circular(5),
               ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: <Widget>[
-                  left,
-                  right,
+              child: Text(formatedTime(video.duration!),
+                  style: style.BannerVideo.time),
+            );
+
+            final downloadButton = Container(
+              decoration: const BoxDecoration(
+                gradient: style.BannerVideo.gradientDownload,
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                color: style.BannerVideo.colorIconDownload,
+                onPressed: () {},
+                icon: const Icon(Icons.download),
+              ),
+            );
+
+            final left = Expanded(
+              flex: 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(video.title, style: style.BannerVideo.title),
+                  const SizedBox(height: 10),
+                  Text(video.author, style: style.BannerVideo.channel),
                 ],
               ),
-            ),
-          ),
-        ],
-      ),
-    );
+            );
+
+            final right = Expanded(
+              child: Column(
+                children: [
+                  downloadButton,
+                  const Spacer(),
+                  timeBox,
+                ],
+              ),
+            );
+
+            return Container(
+              width: 480,
+              height: 220,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                image: DecorationImage(
+                  image: NetworkImage(video.thumbnails.standardResUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.fromLTRB(16, 16, 0, 8),
+                      child: Row(
+                        children: <Widget>[
+                          left,
+                          right,
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else if (snapshot.hasError) {
+            final body = Column(
+              children: [
+                Image.asset('assets/meteor.png', width: width / 1.7),
+                const SizedBox(height: 25),
+                const Text(
+                  'Vídeo não encontrado.\nTem tem certeza que digitou certo?',
+                  style: style.BannerVideo.badAnswer,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            );
+
+            return Center(child: body);
+          } else {
+            var body = Column(
+              children: <Widget>[
+                SizedBox(
+                  width: width / 2,
+                  height: width / 2,
+                  child: CircularProgressIndicator(
+                    strokeWidth: width / 30,
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text('Aguarde um momento...',
+                    style: style.BannerVideo.searching),
+              ],
+            );
+            return Center(child: body);
+          }
+        });
   }
 }
--- lib/styles/banner_video.dart
+++ lib/styles/banner_video.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 
 class BannerVideo {
   static const title = TextStyle(
@@ -9,9 +11,35 @@ class BannerVideo {
   );
 
   static final channel = TextStyle(
-    color: Colors.white.withOpacity(0.4),
+    color: Colors.white.withOpacity(0.5),
     fontSize: 19,
     letterSpacing: 1,
+  );
+
+  static const time = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 17,
+    letterSpacing: 1.5,
+  );
+
+  static const gradientDownload = LinearGradient(
+    colors: [
+      Color.fromRGBO(186, 83, 212, 1),
+      Color.fromRGBO(89, 34, 148, 1),
+    ],
+    begin: Alignment.bottomLeft,
+    end: Alignment.topRight,
+  );
+
+  static const searching = TextStyle(
+    color: Colors.white,
+    fontSize: 19,
+  );
+
+  static const badAnswer = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
   );
 
   static const colorIconDownload = Colors.white;
