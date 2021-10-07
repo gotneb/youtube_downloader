76887f21eb2a87b38c8799d242bfd3e5817890f4
Gabriel Bento da Silva <67703889+HDG-Gabriel@users.noreply.github.com>
2021-10-07 16:41:07 -0400

    Add path_provider and changed main UI

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -22,7 +22,7 @@ class _HomeState extends State<Home> {
       const Text('Downloader', style: style.Home.title),
       const SizedBox(height: 15),
       search,
-      const SizedBox(height: 15),
+      const SizedBox(height: 45),
     ]);
   }
 
