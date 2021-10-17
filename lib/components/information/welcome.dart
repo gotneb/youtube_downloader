5f7b08d4bde76d0c2c862258b7d22ea47c30a50b
Gabriel Bento da Silva <67703889+HDG-Gabriel@users.noreply.github.com>
2021-10-17 15:04:33 -0400

    Update welcome.dart
    Added rotation transition

--- lib/components/information/welcome.dart
+++ lib/components/information/welcome.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Welcome extends StatelessWidget {
+class Welcome extends StatefulWidget {
   const Welcome({Key? key}) : super(key: key);
 
   static const _style = TextStyle(
@@ -9,17 +9,47 @@ class Welcome extends StatelessWidget {
   );
 
   @override
+  _WelcomeState createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> with TickerProviderStateMixin {
+  late AnimationController _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animation = AnimationController(
+      duration: const Duration(seconds: 25),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 
-    return Column(
+    final body = Column(
       children: <Widget>[
-        Center(child: Image.asset('assets/black-hole.png', width: 0.6 * width)),
+        Center(
+          child: RotationTransition(
+            turns: _animation,
+            alignment: Alignment.center,
+            child: Image.asset('assets/black-hole.png', width: 0.6 * width),
+          ),
+        ),
         const SizedBox(height: 30),
-        const Text('Nenhum download feito', style: _style),
+        const Text('Nenhum download feito', style: Welcome._style),
         const SizedBox(height: 10),
-        const Text('Que tal iniciar um ?', style: _style),
+        const Text('Que tal iniciar um ?', style: Welcome._style),
       ],
     );
+
+    return body;
+  }
+
+  @override
+  void dispose() {
+    _animation.dispose();
+    super.dispose();
   }
 }
