7ad93c9a22ad299df37622252c79d31680e84e66
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2023-11-08 10:46:55 -0400

    style: clean code for `App` widget

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_floating_bottom_bar/flutter_floating_bottom_bar.dart';
+import 'package:youtube_downloader/constants.dart';
 import 'package:youtube_downloader/views/downloads.dart';
 import 'package:youtube_downloader/views/home.dart';
 
@@ -11,6 +12,8 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> with SingleTickerProviderStateMixin {
+  static const rowHeight = 40.0;
+
   late int currentPage;
   late TabController tabController;
 
@@ -35,54 +38,49 @@ class _AppState extends State<App> with SingleTickerProviderStateMixin {
     });
   }
 
+  Widget _buildIconTab({
+    required IconData icon,
+    required int index,
+  }) =>
+      Container(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        height: rowHeight,
+        width: double.infinity,
+        decoration: ShapeDecoration(
+          color: currentPage == index ? Colors.red : Colors.transparent,
+          shape: const StadiumBorder(),
+        ),
+        child: Center(child: Icon(icon, color: Colors.blue)),
+      );
+
   @override
   Widget build(BuildContext context) {
+    const tabs = [HomeView(), DownloadsView()];
+
+    final tabbar = TabBar(
+      indicatorColor: Colors.transparent,
+      enableFeedback: false,
+      splashBorderRadius: BorderRadius.circular(borderRadius),
+      controller: tabController,
+      tabs: [
+        _buildIconTab(icon: Icons.home_rounded, index: 0),
+        _buildIconTab(icon: Icons.subscriptions_rounded, index: 1),
+      ],
+    );
+
+    final bottomBar = BottomBar(
+      borderRadius: BorderRadius.circular(borderRadius),
+      barColor: Colors.white,
+      body: (_, controller) => TabBarView(
+        controller: tabController,
+        children: tabs,
+      ),
+      child: tabbar,
+    );
+
     return SafeArea(
       child: Scaffold(
-        body: BottomBar(
-          borderRadius: BorderRadius.circular(32),
-          barColor: Colors.white,
-          body: (context, controller) => TabBarView(
-            controller: tabController,
-            children: [HomeView(), DownloadsView()],
-          ),
-          child: TabBar(
-            indicatorColor: Colors.transparent,
-            enableFeedback: false,
-            splashBorderRadius: BorderRadius.circular(32),
-            controller: tabController,
-            tabs: [
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                height: 40,
-                width: double.infinity,
-                decoration: ShapeDecoration(
-                  color: currentPage == 0 ? Colors.red : Colors.transparent,
-                  shape: StadiumBorder(),
-                ),
-                child: Center(
-                    child: Icon(
-                  Icons.home_rounded,
-                  color: Colors.blue,
-                )),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                height: 40,
-                width: double.infinity,
-                decoration: ShapeDecoration(
-                  color: currentPage == 1 ? Colors.green : Colors.transparent,
-                  shape: StadiumBorder(),
-                ),
-                child: Center(
-                    child: Icon(
-                  Icons.subscriptions_rounded,
-                  color: Colors.blue,
-                )),
-              ),
-            ],
-          ),
-        ),
+        body: bottomBar,
       ),
     );
   }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
 import 'package:logger/logger.dart';
 
+
+const borderRadius = 32.0;
 final logger = Logger();
